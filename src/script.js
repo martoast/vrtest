2d1529851e34d9b808488804bb6e62514e9486a5
Alejandro Martos <alexmartos96@gmail.com>
2022-06-27 21:44:39 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,242 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
+import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory.js";
+
+let controller, controllerGrip;
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+//GRASS TEXTURES
+const grassColorTexture = textureLoader.load("/textures/grass/color.jpg");
+const grassAmbientOcclusionTexture = textureLoader.load(
+  "/textures/grass/ambientOcclusion.jpg"
+);
+const grassNormalTexture = textureLoader.load("/textures/grass/normal.jpg");
+const grassRoughnessTexture = textureLoader.load(
+  "/textures/grass/roughness.jpg"
+);
+
+grassColorTexture.repeat.set(8, 8);
+grassAmbientOcclusionTexture.repeat.set(8, 8);
+grassNormalTexture.repeat.set(8, 8);
+grassRoughnessTexture.repeat.set(8, 8);
+
+grassColorTexture.wrapS = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+grassNormalTexture.wrapS = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
+
+grassColorTexture.wrapT = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+grassNormalTexture.wrapT = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
+
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({
+    map: grassColorTexture,
+    aoMap: grassAmbientOcclusionTexture,
+    normalMap: grassNormalTexture,
+    roughnessMap: grassRoughnessTexture,
+  })
+);
+floor.geometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
+);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = 0;
+scene.add(floor);
+
+// global lights
+const lightSun = new THREE.DirectionalLight(0xeeffff);
+lightSun.name = "sun";
+lightSun.position.set(0.2, 1, 0.1);
+const lightFill = new THREE.DirectionalLight(0xfff0ee, 0.3);
+lightFill.name = "fillLight";
+lightFill.position.set(-0.2, -1, -0.1);
+
+scene.add(lightSun, lightFill);
+
+/**
+ * CUBE Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.y = 1;
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Fullscreen
+ */
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4, 2, 5);
+
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//VR button
+document.body.appendChild(VRButton.createButton(renderer));
+
+renderer.xr.enabled = true;
+
+//controllers
+controller = renderer.xr.getController(0);
+controller.addEventListener("selectstart", onSelectStart);
+controller.addEventListener("selectend", onSelectEnd);
+controller.addEventListener("connected", function (event) {
+  this.add(buildController(event.data));
+});
+controller.addEventListener("disconnected", function () {
+  this.remove(this.children[0]);
+});
+
+scene.add(controller);
+
+const controllerModelFactory = new XRControllerModelFactory();
+
+controllerGrip = renderer.xr.getControllerGrip(0);
+controllerGrip.add(
+  controllerModelFactory.createControllerModel(controllerGrip)
+);
+scene.add(controllerGrip);
+
+function onSelectStart() {
+  this.userData.isSelecting = true;
+}
+
+function onSelectEnd() {
+  this.userData.isSelecting = false;
+}
+
+function buildController(data) {
+  let geometry, material;
+
+  switch (data.targetRayMode) {
+    case "tracked-pointer":
+      geometry = new THREE.BufferGeometry();
+      geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, -1], 3)
+      );
+      geometry.setAttribute(
+        "color",
+        new THREE.Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3)
+      );
+
+      material = new THREE.LineBasicMaterial({
+        vertexColors: true,
+        blending: THREE.AdditiveBlending,
+      });
+
+      return new THREE.Line(geometry, material);
+
+    case "gaze":
+      geometry = new THREE.RingGeometry(0.02, 0.04, 32).translate(0, 0, -1);
+      material = new THREE.MeshBasicMaterial({
+        opacity: 0.5,
+        transparent: true,
+      });
+      return new THREE.Mesh(geometry, material);
+  }
+}
+
+renderer.xr.addEventListener("sessionstart", () => {
+  renderer.xr.getCamera().position.copy(camera.position);
+
+  renderer.xr.getCamera().lookAt(camera.position);
+  console.log(camera.position, renderer.xr.getCamera().position);
+});
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  renderer.setAnimationLoop(function () {
+    renderer.render(scene, camera);
+  });
+};
+
+tick();
